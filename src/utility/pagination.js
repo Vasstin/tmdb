ee1e8083875ba1ef96fa4e29f6eedc1b14c47487
Vasstin <vastin115408@gmail.com>
2022-08-30 17:34:10 +0300

    fixed width

--- src/utility/pagination.js
+++ src/utility/pagination.js
@@ -4,7 +4,7 @@ import Stack from "@mui/material/Stack";
 
 const BasicPagination = (props) => {
   return (
-    <Stack sx={{ margin: "0 auto" }} spacing={2}>
+    <Stack sx={{ margin: "0 auto", minWidth: '350px' }} spacing={2}>
       <Pagination
         count={props.totalPages}
         page={props.currentPage}
