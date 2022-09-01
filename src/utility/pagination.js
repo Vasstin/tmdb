888216749b4eb7036a5d7f2f7197df9dde19e58c
Vasstin <vastin115408@gmail.com>
2022-09-01 11:30:40 +0300

    centering paggination item

--- src/utility/pagination.js
+++ src/utility/pagination.js
@@ -4,8 +4,9 @@ import Stack from "@mui/material/Stack";
 
 const BasicPagination = (props) => {
   return (
-    <Stack sx={{ margin: "0 auto", minWidth: '350px' }} spacing={2}>
+    <Stack sx={{ margin: "0 auto", minWidth: "350px" }} spacing={2}>
       <Pagination
+        sx={{ margin: "0 auto" }}
         count={props.totalPages}
         page={props.currentPage}
         onChange={(event, value) => props.pageChanger(event, value)}
